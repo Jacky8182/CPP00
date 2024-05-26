feef39f56071897aa3cb1d1dcb15f58f142c17ef
Jacky8182 <125771703+Jacky8182@users.noreply.github.com>
2024-05-26 16:30:30 +0200

    Create Contact.setter.cpp

---
+++ src/contact/Contact.setter.cpp
@@ -0,0 +1,26 @@
+#include "PhoneBook.class.hpp"
+
+void setName(std::string &Name)
+{
+	this->_Name = Name;
+}
+
+void setFirstName(std::string &FirstName)
+{
+	this->_FirstName = FirstName;
+}
+
+void setNickName(std::string &NickName)
+{
+	this->_NickName = NickName;
+}
+
+void setPhoneNumber(std::string &PhoneNumber)
+{
+	this->_PhoneNumber = PhoneNumber;
+}
+
+void setDarkestSecret(std::string &DarkestSecret)
+{
+	this->_DarkestSecret = DarkestSecret;
+}
