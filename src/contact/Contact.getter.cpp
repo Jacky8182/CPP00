7a5399436f76534081cd13fb28511d43edd7ce88
Jacky8182 <125771703+Jacky8182@users.noreply.github.com>
2024-05-26 16:30:05 +0200

    Create Contact.getter.cpp

---
+++ src/contact/Contact.getter.cpp
@@ -0,0 +1,26 @@
+#include "PhoneBook.class.hpp"
+
+std::string getName() const
+{
+	return this->_Name;
+}
+
+std::string getFirstName() const
+{
+	return this->_FirstName;
+}
+
+std::string getNickName() const
+{
+	return this->_NickName;
+}
+
+std::string getPhoneNumber() const
+{
+	return this->_PhoneNumber;
+}
+
+std::string getDarkestSecret() const
+{
+	return this->_DarkestSecret;
+}
