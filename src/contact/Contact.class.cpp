3fe301bdc45bbe2c129f07f697c992f101900465
Jacky8182 <125771703+Jacky8182@users.noreply.github.com>
2024-05-26 16:29:35 +0200

    Create Contact.class.cpp

---
+++ src/contact/Contact.class.cpp
@@ -0,0 +1,11 @@
+#include "PhoneBook.class.hpp"
+
+Contact::Contact()
+{
+	std::cout << "Constructor called" << std::endl;
+}
+
+~Contact::Contact()
+{
+	std::cout << "Destructor called" << std::endl;
+}
