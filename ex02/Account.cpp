c86df9375439f08d9c4c2fc5fedfe3f31c320c0c
Jacky Ung <jaung@made-f0Br3s1.clusters.42paris.fr>
2024-06-03 13:48:56 +0200

    update

---
+++ ex02/Account.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaung <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/03 13:18:37 by jaung             #+#    #+#             */
+/*   Updated: 2024/06/03 13:18:38 by jaung            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "Account.hpp"
+
+Account::Account(int initial_deposit)
+{
+}
+
+Account::Account()
+{
+}
+
+Account::~Account()
+{
+}
+
+void Account::makeDeposit(int deposit)
+{
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+}
+
+int Account::checkAmount(void) const
+{
+}
+
+void Account::displayStatus(void) const
+{
+}
